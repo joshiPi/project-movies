84d993a5038567c1589c89a585d2a00d09707b81
joshiPi <bcasprabhatjoshi@gmail.com>
2021-10-27 22:19:29 +0530

    home-part-complete

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import {encode} from 'querystring';
 import Header from "../../common/header/Header";
 import {
   GridListTileBar,
@@ -9,18 +10,21 @@ import {
   CardHeader,
   FormControl,
   InputLabel,
-  FormHelperText,
   Input,
   Select,
   MenuItem,
   TextField,
   Button,
+  Checkbox,
+  ListItemText,
 } from "@material-ui/core";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 import "./Home.css";
 
 const Home = () => {
   const [data, setData] = useState(null);
+  const [dataRelease, setDataRelease] = useState(null);
+  const [callAgain, setCallAgain] = useState(true);
   useEffect(() => {
     (function () {
       fetch("/api/v1/movies")
@@ -29,14 +33,65 @@ const Home = () => {
         .catch((err) => console.log(err));
     })();
   }, []);
-  // useEffect(() => {
-
-  // })
-  const [age, setAge] = React.useState("");
-  const [username, setUsername] = useState("");
+  const [genres, setGenres] = useState([]);
+  useEffect(() => {
+    (function () {
+      fetch("/api/v1/genres?limit=999")
+        .then((data) => data.json())
+        .then((data) => setGenres(data.genres))
+        .catch((err) => console.log(err));
+    })();
+  }, []);
+  const [artists, setArtist] = useState([]);
+  useEffect(() => {
+    (function () {
+      fetch("/api/v1/artists?limit=999")
+        .then((data) => data.json())
+        .then((data) => setArtist(data.artists))
+        .catch((err) => console.log(err));
+    })();
+  }, []);
+  useEffect(() => {
+    (function () {
+      const opts = {
+        params: {
+          genre: filterGenres.toString(),
+          artists: filterArtist.toString(),
+          title: movieName,
+          start_date: releaseDate,
+          end_dat: releaseDateEnd,
+        },
+      };
+      console.log(encode(opts.params))
+      fetch(`/api/v1/movies?${encode(opts.params)}`)
+        .then((data) => data.json())
+        .then((data) => setDataRelease(data.movies))
+        .catch((err) => console.log(err));
+    })();
+  }, [callAgain]);
+  const [filterGenres, setFilterGenres] = useState([]);
+  const [filterArtist, setFilterArtist] = useState([]);
+  const [movieName, setMovieName] = useState("");
+  const [releaseDate, setReleaseDate] = useState("");
+  const [releaseDateEnd, setReleaseDateEnd] = useState("");
 
   const handleChange = (event) => {
-    setAge(event.target.value);
+    const {
+      target: { value },
+    } = event;
+    setFilterGenres(
+      // On autofill we get a the stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
+  };
+  const handleChangeArtist = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setFilterArtist(
+      // On autofill we get a the stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
   };
   return (
     <div className="home">
@@ -46,87 +101,124 @@ const Home = () => {
         {data && (
           <GridList className="gridList" cellHeight={250}>
             {data &&
-              data.filter(m => m.status === 'RELEASED').map((tile) => (
-                <Link to={`/movie/${tile.id}`} style={{ width: 'inherit', height: 'inherit'}}>
-                <GridListTile key={tile.id} style={{ width: "inherit" }}>
-                  <img
-                    className="grid-image"
-                    src={tile.poster_url}
-                    alt={tile.title}
-                  />
-                  <GridListTileBar title={tile.title} />
-                </GridListTile>
-                </Link>
-              ))}
+              data
+                .filter((m) => m.status === "RELEASED")
+                .map((tile) => (
+                  <Link
+                    to={`/movie/${tile.id}`}
+                    style={{ width: "inherit", height: "inherit" }} key={tile.id}
+
+                  >
+                    <GridListTile style={{ width: "inherit" }}>
+                      <img
+                        className="grid-image"
+                        src={tile.poster_url}
+                        alt={tile.title}
+                      />
+                      <GridListTileBar title={tile.title} />
+                    </GridListTile>
+                  </Link>
+                ))}
           </GridList>
         )}
       </div>
       <div className="upcoming-movies">
         <div className="filtered-movie-section">
-          {data && (
+          {dataRelease && (
             <GridList cellHeight={350} cols={4}>
-              {data &&
+              {dataRelease &&
                 // data.filter(m => m.status === 'PUBLISHED')
-                data.map((tile) => (
-                  <Link to={`/movie/${tile.id}`} style={{ width: 'inherit', height: 'inherit'}}>
-                  <GridListTile
-                    key={tile.id}
-                    style={{ margin: "16px 16px 0 0" }}
-                  >
-                    <img src={tile.poster_url} alt={tile.title} />
-                    <GridListTileBar title={tile.title} />
-                  </GridListTile>
-                  </Link>
-                ))}
+                dataRelease
+                  // .filter((m) => m.status === "PUBLISHED")
+                  .map((tile) => (
+                    <Link
+                      to={`/movie/${tile.id}`}
+                      style={{ width: "inherit", height: "inherit" }}
+                      key={tile.id}
+                    >
+                      <GridListTile
+                        style={{ margin: "16px 16px 0 0" }}
+                      >
+                        <img src={tile.poster_url} alt={tile.title} />
+                        <GridListTileBar title={tile.title} />
+                      </GridListTile>
+                    </Link>
+                  ))}
             </GridList>
           )}
         </div>
-        <div className="filter-movies">
+        <div className="filter-movies" style={{ minWidth: "240px" }}>
           <Card>
             <CardContent>
               <CardHeader title={"FIND MOVIES BY:"} />
-              <FormControl fullWidth>
+              <FormControl fullWidth style={{ padding: "10px 0" }}>
                 <InputLabel htmlFor="my-input">Movie Name</InputLabel>
                 <Input
                   required
                   name="firstName"
                   aria-describedby="my-helper-text"
+                  value={movieName}
+                  onChange={(e) => setMovieName(e.target.value)}
                 />
               </FormControl>
-              <FormControl fullWidth>
+              <FormControl fullWidth style={{ padding: "10px 0" }}>
                 <InputLabel variant="standard" htmlFor="uncontrolled-native">
-                  Age
+                  Genres
+                </InputLabel>
+                <Select
+                  multiple
+                  labelid="demo-simple-select-standard-label"
+                  id="demo-simple-select-standard"
+                  value={filterGenres}
+                  onChange={handleChange}
+                  renderValue={(selected) => selected.join(", ")}
+                  label="Genres"
+                >
+                  {genres.map((genre) => (
+                    <MenuItem key={genre.id} value={genre.genre}>
+                      <Checkbox
+                        checked={filterGenres.indexOf(genre.genre) > -1}
+                      />
+                      <ListItemText primary={genre.genre} />
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              <FormControl fullWidth style={{ padding: "10px 0" }}>
+                <InputLabel variant="standard" htmlFor="uncontrolled-native">
+                  Artists
                 </InputLabel>
                 <Select
                   labelid="demo-simple-select-standard-label"
                   id="demo-simple-select-standard"
-                  value={age}
-                  onChange={handleChange}
+                  multiple
+                  value={filterArtist}
+                  renderValue={(selected) => selected.join(", ")}
+                  onChange={handleChangeArtist}
                   label="Genres"
                 >
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
+                  {artists.map((artist) => (
+                    <MenuItem
+                      key={artist.id}
+                      value={artist.first_name + " " + artist.last_name}
+                    >
+                      <Checkbox
+                        checked={
+                          filterArtist.indexOf(
+                            artist.first_name + " " + artist.last_name
+                          ) > -1
+                        }
+                      />
+                      <ListItemText
+                        primary={artist.first_name + " " + artist.last_name}
+                      />
+                    </MenuItem>
+                  ))}
                 </Select>
               </FormControl>
-              <FormControl fullWidth>
-                <InputLabel variant="standard" htmlFor="uncontrolled-native">
-                  Age
-                </InputLabel>
-                <Select
-                  labelid="demo-simple-select-standard-label"
-                  id="demo-simple-select-standard"
-                  value={age}
-                  onChange={handleChange}
-                  label="Genres"
-                >
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-              </FormControl>
-              <FormControl style={{ display: "block", paddingBottom: "20px" }}>
+              <FormControl style={{ display: "block", padding: "20px 0" }}>
                 <TextField
+                  fullWidth
                   id="standard-number"
                   label="Release Start Date"
                   type="date"
@@ -134,10 +226,12 @@ const Home = () => {
                     shrink: true,
                   }}
                   variant="standard"
+                  onChange={(e) => setReleaseDate(e.target.value)}
                 />
               </FormControl>
               <FormControl style={{ display: "block", paddingBottom: "20px" }}>
                 <TextField
+                  fullWidth
                   id="standard-number"
                   label="Release End Date"
                   type="date"
@@ -145,9 +239,16 @@ const Home = () => {
                     shrink: true,
                   }}
                   variant="standard"
+                  onChange={(e) => setReleaseDateEnd(e.target.value)}
                 />
               </FormControl>
-              <Button variant="contained" color="primary" type="submit">
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                fullWidth
+                onClick={() => setCallAgain(!callAgain)}
+              >
                 APPLY
               </Button>
             </CardContent>
